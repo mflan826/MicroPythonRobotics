3b81c64c05330719cdd7c2559714c772d1d0071d
mflan826 <94853197+mflan826@users.noreply.github.com>
2021-12-31 19:34:29 -0500

    Add files via upload

---
+++ BlinkLED.py
@@ -0,0 +1,18 @@
+from machine import Pin
+# get the config library for Pic
+import time
+
+led = Pin(25, Pin.OUT)
+# create an object configured to the Pin with the Pico's LED
+
+# while loop for blinking the LED
+while True:
+    led(1)
+    print("LED IS ON")
+    time.sleep(1)
+    led(0)
+    print("LED IS OFF")
+    time.sleep(1)
+
+# While in REPL, use control c to turn the program off or control d to start
+
